370e814105be24e89d43755c427dc679ac8c975e
GeorgeLiuXin <liuxinindiegame@gmail.com>
2019-08-29 13:20:33 +0800

    技能飘字

--- Assets/GameMain/Scripts/GameLogic/CombatSystem/Combat/HUDTextManager.cs
+++ Assets/GameMain/Scripts/GameLogic/CombatSystem/Combat/HUDTextManager.cs
@@ -21,13 +21,13 @@ namespace Galaxy
 			HUDRoot = null;
 		}
 
-        protected void OnShowHUDText(object sender, GameEventArgs e)
-        {
-            SkillEffectEvent ne = (SkillEffectEvent)e;
-            if (ne == null)
-                return;
-            Entity pCaster = GameEntry.Entity.GetGameEntity(ne.CasterID);
-            if (pCaster == null)
+		protected void OnShowHUDText(object sender, GameEventArgs e)
+		{
+			SkillEffectEvent ne = (SkillEffectEvent)e;
+			if(ne == null)
+				return;
+			Entity pCaster = GameEntry.Entity.GetGameEntity(ne.CasterID);
+			if(pCaster == null)
 				return;
 			Entity pTarget = GameEntry.Entity.GetGameEntity(ne.TargetID);
 			if(pTarget == null)
@@ -37,11 +37,15 @@ namespace Galaxy
 			{
 				Vector3 vOffset = pTarget.GetPos() - pCaster.GetPos();
 				vOffset.y = 0;
-				Vector3 vRight = pCaster.CachedTransform.right;
+
+				Camera pCamera = GameEntry.CameraMgr.GetCurEngineCamera();
+				if(pCamera == null || pCamera.transform == null)
+					return;
+				Vector3 vRight = pCamera.transform.right;
 				vRight.y = 0;
 
 				float dot = Vector3.Dot(vOffset, vRight);
-				bl_Guidance dir = dot > 0 ? bl_Guidance.LeftDown : bl_Guidance.RightDown;
+				bl_Guidance dir = dot <= 0 ? bl_Guidance.LeftDown : bl_Guidance.RightDown;
 
 				HUDRoot.NewText(Utility.Text.Format("- {0:N1}", ne.EffectValue), pTarget.CachedTransform, Color.red,
 					8, 20f, -1f, 2.2f, dir);
